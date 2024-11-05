80fdeced385a2eb8bef1f969c3afbc71ea8309f6
M.Chaitanya Sai <marvelchaitanya@gmail.com>
2024-11-05 14:50:42 +0530

    Create SimpleCalculator.py

---
+++ SimpleCalculator.py
@@ -0,0 +1,15 @@
+n=int(input("ENTER THE NUMBER:"))
+d=int(input("ENTER THE NUMBER:"))
+s=input("OPERATIPON:")
+if(s=="+"):
+    print("Answer=",n+d)
+elif(s=="-"):
+    print("Answer=",n-d)
+elif(s=="*"):
+    print("Answer=",n*d)
+elif(s=="/"):
+    print("Answer=",n/d)
+elif(s=="//"):
+    print("Answer=",n//d)
+elif(s=="%"):
+    print("Answer=",n%d)
