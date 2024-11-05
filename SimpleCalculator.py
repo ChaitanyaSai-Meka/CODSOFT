c2dedf75ebe9233e8f031e15a66585a20671952a
M.Chaitanya Sai <marvelchaitanya@gmail.com>
2024-11-05 17:59:48 +0530

    Update SimpleCalculator.py

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -1,6 +1,6 @@
-n=int(input("ENTER THE NUMBER:"))
-d=int(input("ENTER THE NUMBER:"))
-s=input("OPERATIPON:")
+n=int(input("Enter a Number:"))
+d=int(input("Enter a Number:"))
+s=input("Operator:")
 if(s=="+"):
     print("Answer=",n+d)
 elif(s=="-"):
@@ -13,3 +13,5 @@ elif(s=="//"):
     print("Answer=",n//d)
 elif(s=="%"):
     print("Answer=",n%d)
+else:
+    print("Please Enter a Valid Operator")
